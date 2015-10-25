adf84ab85ba419a0f1041652e20d4c9629150e6b
Adniwhack <rasnayake.kavinda@gmail.com>
2015-10-25 18:59:06 +0530

    Added solution for Mr. Pippos Pizza

---
+++ IEEExtreme9/Mr.Pippos_Pizza.py
@@ -0,0 +1,33 @@
+def triangles(n):
+    if n <= 3:
+        return 1
+    else:
+        m = n-2
+    
+        res = fact(2*m) // (fact( m + 1) * fact(m))
+        return res
+    
+def fact(n):
+    if n<=0:
+        return 1
+    else:
+        x = 1
+        for i in range(n):
+            x *= i+1
+        return x
+
+
+for i in range(100):
+    x = input()
+    if (x != ""):
+        x = int(x)
+        n = 3
+        while (n > 0):
+            y = triangles(n)
+            if (x == y):
+                print(n)
+                break
+            else:
+                n+= 1
+    else:
+        break
